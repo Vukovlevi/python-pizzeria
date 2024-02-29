1d22cb19adc3634d8f507cba8931abe710ac7619
Szabó-Vukov Levente <11c-szabo-vukov@IPARI.SULINET.HU>
2024-02-29 08:18:40 +0100

    initial commit - hello world

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title("A fekete ingesek Pizzéria")
+window.geometry("1920x1080")
+
+frame = tk.Label(window, text="Hello World")
+frame.pack(fill=tk.X, expand=tk.Y)
+
+window.mainloop()
