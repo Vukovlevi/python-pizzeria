4383e5f8a142144f36aa4089dfbf2367e7de4923
Simon Zsombor <11c-simon@IPARI.SULINET.HU>
2024-03-07 08:41:36 +0100

    Price corrections and Img solution

--- main.py
+++ main.py
@@ -14,18 +14,20 @@ window = tk.Tk()
 window.title("A fekete ingesek Pizzéria")
 window.geometry("1920x1080")
 
-for i in range(0,len(pizzak)):
+pizzaPhotos=[]
+for i, pizza in enumerate(pizzak):
     pizzaFrame=tk.Frame(window,borderwidth=2,relief="raised",padx=20)
-    pizzaPhoto=tk.PhotoImage(file="./Img/placeholder.png",width=200,height=100)
+    pizzaPhoto=tk.PhotoImage(file=pizza.img,width=200,height=100)
+    pizzaPhotos.append(pizzaPhoto)
     pizzaPhotoPlace=tk.Label(pizzaFrame,image=pizzaPhoto)
     pizzaPhotoPlace.pack()
-    pizzaName=tk.Label(pizzaFrame,text=pizzak[i].name,pady=5)
+    pizzaName=tk.Label(pizzaFrame,text=pizza.name,pady=5)
     pizzaName.pack()
-    pizzaComp=tk.Label(pizzaFrame,text=pizzak[i].components,pady=5)
+    pizzaComp=tk.Label(pizzaFrame,text=pizza.components,pady=5)
     pizzaComp.pack()
-    pizzaRecipe=tk.Label(pizzaFrame,text=pizzak[i].recipe,pady=5)
+    pizzaRecipe=tk.Label(pizzaFrame,text=pizza.recipe,pady=5)
     pizzaRecipe.pack()
-    pizzaOrder=tk.Button(pizzaFrame,text="Rendelés",pady=5)
+    pizzaOrder=tk.Button(pizzaFrame,text=f"Rendelés - {pizza.price}Ft", pady=5)
     pizzaOrder.pack()
     pizzaFrame.grid(row=0,column=i,padx=100,pady=50)
 
