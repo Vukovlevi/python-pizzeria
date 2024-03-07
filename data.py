b43751e047a7b0f030c4e761f1a4da611106ff93
Szabó-Vukov Levente <11c-szabo-vukov@IPARI.SULINET.HU>
2024-03-07 07:49:47 +0100

    feat: added menu + img to pizza type

--- data.py
+++ data.py
@@ -1,10 +1,10 @@
 from type import Pizza
 from type import User
 
-pizza1 = Pizza("Margaréta", "paradicsom szósz, sajt, oregano", "Meg kell csinálni az összetevők segítségével!", "paradicsomszósz,mozarella,sonka,kukorica", 2390)
-pizza2 = Pizza("Kukoricás", "paradicsom szósz, kukorica, sajt, oregano", "Meg kell csinálni az összetevők segítségével!", "paradicsomszósz,mozarella,sonka,kukorica", 2390)
-pizza3 = Pizza("Sonkás", "paradicsom szósz, sonka, sajt, oregano", "Meg kell csinálni az összetevők segítségével!", "paradicsomszósz,mozarella,sonka,kukorica", 2390)
-pizza4 = Pizza("Bolognai", "bolognai szósz, paradicsom szósz, sajt, oregano", "Meg kell csinálni az összetevők segítségével!", "paradicsomszósz,mozarella,sonka,kukorica", 2390)
+pizza1 = Pizza("Margaréta", "paradicsom szósz, sajt, oregano", "Meg kell csinálni az összetevők segítségével!", "paradicsomszósz,mozarella,sonka,kukorica", 2390, "Img/Margareta.png")
+pizza2 = Pizza("Kukoricás", "paradicsom szósz, kukorica, sajt, oregano", "Meg kell csinálni az összetevők segítségével!", "paradicsomszósz,mozarella,sonka,kukorica", 2390, "Img/Kukoricas.png")
+pizza3 = Pizza("Sonkás", "paradicsom szósz, sonka, sajt, oregano", "Meg kell csinálni az összetevők segítségével!", "paradicsomszósz,mozarella,sonka,kukorica", 2390, "Img/Sonkas.png")
+pizza4 = Pizza("Bolognai", "bolognai szósz, paradicsom szósz, sajt, oregano", "Meg kell csinálni az összetevők segítségével!", "paradicsomszósz,mozarella,sonka,kukorica", 2390, "Img/Bolognai.png")
 pizzak = [pizza1, pizza2, pizza3, pizza4]
 
 user = User("Gipsz Jakab", "admin", "admin", "8200 Veszprém, Lóczy Lajos utca 12/B 1/1", "+36301564872", "gipsz.jakab@gmail.com")
--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import tkinter as tk
+from tkinter import messagebox
+
+def confirm_quit():
+    sure = messagebox.askyesno("Megerősítés", "Biztosan ki akar lépni?")
+    if sure:
+        window.quit()
+
+def about():
+    messagebox.showinfo("Készítők", "Simon Zsombor\nSzabó-Vukov Levente")
 
 window = tk.Tk()
 window.title("A fekete ingesek Pizzéria")
@@ -18,18 +27,18 @@ pizzaOrder=tk.Button(pizzaFrame,text="Placeholder",pady=5)
 pizzaOrder.pack()
 pizzaFrame.grid(row=0,column=0,padx=100,pady=50)
 
-pizzaFrame=tk.Frame(window,borderwidth=2,relief="raised",padx=20)
-pizzaPhoto=tk.PhotoImage(file="./Img/placeholder.png",width=200,height=100)
-pizzaPhotoPlace=tk.Label(pizzaFrame,image=pizzaPhoto)
-pizzaPhotoPlace.pack()
-pizzaName=tk.Label(pizzaFrame,text="Placeholder",pady=5)
-pizzaName.pack()
-pizzaComp=tk.Label(pizzaFrame,text="Placeholder",pady=5)
-pizzaComp.pack()
-pizzaRecipe=tk.Label(pizzaFrame,text="Placeholder",pady=5)
-pizzaRecipe.pack()
-pizzaOrder=tk.Button(pizzaFrame,text="Placeholder",pady=5)
-pizzaOrder.pack()
-pizzaFrame.grid(row=0,column=1,padx=100,pady=50)
+order_menu = tk.Menu()
+order_menu.add_command(label="Rendelés hozzáadása")
+order_menu.add_command(label="Összes rendelés")
 
+general_menu = tk.Menu()
+general_menu.add_command(label="Névjegy", command=about)
+general_menu.add_command(label="Kilépés", command=confirm_quit)
+
+menu = tk.Menu(window)
+menu.add_cascade(label="Rendelés", menu=order_menu)
+menu.add_cascade(label="Általános", menu=general_menu)
+menu.add_command(label="Kijelentkezés")
+
+window.config(menu=menu)
 window.mainloop()
